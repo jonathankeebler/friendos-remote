cb1187690edc926c48502751acdf9d4e1df14046
Jonathan Keebler <jonathan@keebler.net>
2017-04-06 18:07:15 -0400

    Basic implementation of friendos proxy

---
+++ app.js
@@ -0,0 +1,15 @@
+var FriendOS = require("./lib/index.js");
+
+var app = new FriendOS();
+
+app.start(function(err)
+{
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        
+    }
+}); 
