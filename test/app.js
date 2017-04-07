fcce713e52083c9df3dd04e0c58a6e1b1d3a5732
Jonathan Keebler <jonathan@keebler.net>
2017-04-06 21:59:47 -0400

    Test license. Check config before sending it to the server.

--- test/app.js
+++ test/app.js
@@ -1,6 +1,8 @@
 var FriendOS = require("../lib/index.js");
 
-var app = new FriendOS();
+var app = new FriendOS({
+    license: "1dec9dbf-2c8e-4e7d-9bfd-e31936a5d5e1"
+});
 
 app.start(function(err)
 {
